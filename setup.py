48d889f51187e125c0679847a0b1df25d57a59d1
Marvin Pinto <marvin@pinto.im>
2015-12-19 13:34:42 -0500

    Update the charlesbot-rundeck version from 0.1.0 to 0.2.0

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ test_requirements = [
 
 setup(
     name='charlesbot-rundeck',
-    version='0.1.0',
+    version='0.2.0',
     description="A Charlesbot plugin that integrates with Rundeck",
     long_description=readme,
     author="Marvin Pinto",
