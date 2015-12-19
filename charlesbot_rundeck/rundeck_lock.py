33e6ef2e1e8801e17e6d5d5ede51ce6f976a6448
Marvin Pinto <marvin@pinto.im>
2015-12-19 13:20:02 -0500

    Sad sad lint fixes :(
    I have no idea how this got so bad ...

--- charlesbot_rundeck/rundeck_lock.py
+++ charlesbot_rundeck/rundeck_lock.py
@@ -123,10 +123,10 @@ class RundeckLock(object):
     def get_execution_status_message(self, lock_job):
         """
         Return an appropriate user-facing message
-	"""
-	if lock_job:
-	    return ":lock: Rundeck executions locked by <@%s> :lock:" % self.locked_by_user  # NOQA
-	return ":white_check_mark: Rundeck executions unlocked! :white_check_mark:"  # NOQA
+        """
+        if lock_job:
+            return ":lock: Rundeck executions locked by <@%s> :lock:" % self.locked_by_user  # NOQA
+        return ":white_check_mark: Rundeck executions unlocked! :white_check_mark:"  # NOQA
 
     @asyncio.coroutine
     def set_channel_topic(self, lock_job):
@@ -151,9 +151,9 @@ class RundeckLock(object):
         out_message.append("```")
         for job in self.rundeck_jobs:
             if job.execution_enabled:
-		out_message.append("%s: Unlocked" % job.friendly_name)
+                out_message.append("%s: Unlocked" % job.friendly_name)
             else:
-		out_message.append("%s: Locked" % job.friendly_name)
+                out_message.append("%s: Locked" % job.friendly_name)
         out_message.append("```")
         yield from self.slack.send_channel_message(slack_message.channel,
                                                    "\n".join(out_message))
